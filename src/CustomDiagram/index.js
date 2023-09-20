718d7972e66265f595e36972983bcf2cc204f779
MaMaY2 <919572936@qq.com>
2023-09-20 17:11:57 +0800

    add

--- src/CustomDiagram/index.js
+++ src/CustomDiagram/index.js
@@ -8,19 +8,56 @@ import {
 } from 'react-flow-diagram';
 import model from './model-example';
 import { config, customEntities } from './config-example';
+import axios from 'axios'
+
+const REQUEST_URL = 'http://localhost:8080/api/default'
+const POSITION_URL = 'http://localhost:8080/api/position'
+// const [groups, setGroups] = useState([]);
+
+
 
 class CustomDiagram extends React.PureComponent {
+  constructor(props){
+    super(props)
+    this.state = {
+      modelApi:[],
+      userDiagram: [{ "id": "test", "type": "Task", "width": 110, "height": 110, "x": 110, "y": 110, "name": "test", "linksTo": [{}] }]
+    }
+  }
+
   componentWillMount() {
+     axios.get(REQUEST_URL).then((response) => {
+      this.setState({ modelApi: response.data }, () => {
+        console.log('to Json: ' + JSON.stringify(this.state.modelApi))
+      })
+     })
+     
+    // diagramStore.dispatch(setConfig(config));
+    // diagramStore.dispatch(setEntities(this.state.modelApi));
+
+    // diagramOn('anyChange', entityState =>
+    //   // You can get the model back
+    //   // after modifying the UI representation
+    //   console.info(entityState)
+    // );
+  }
+  render() {
     diagramStore.dispatch(setConfig(config));
-    diagramStore.dispatch(setEntities(model));
+    diagramStore.dispatch(setEntities(this.state.modelApi));
 
     diagramOn('anyChange', entityState =>
       // You can get the model back
       // after modifying the UI representation
-      console.info(entityState)
+      // console.info(entityState)
+      fetch(POSITION_URL, {
+        method: 'post',
+        headers: {
+          'Accept': 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(entityState)
+      })
     );
-  }
-  render() {
     return <Diagram customEntities={customEntities} />;
   }
 }
